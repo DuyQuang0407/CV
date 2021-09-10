4ff2a9d5b8c70d49c56286f7e6eeb4156a8c1633
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-09-10 20:11:31 +0700

    add controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @socials = [
+      { title: 'Twitter', href: "#", icon: 'fab fa-twitter'},
+      { title: 'Facebook', href: "#", icon: 'fab fa-facebook'},
+      { title: 'Instagram', href: "#", icon: 'fab fa-instagram'},
+      { title: 'Github', href: "https://github.com/QuangCD", icon: 'fab fa-github'}
+    ]
   end
 end
