7f5b1cf411cf84f142b9d8c4885293fd74cff04d
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-09-29 20:56:49 +0700

    print to pdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,17 @@
 class HomeController < ApplicationController
   def index
-    # @socials = Social.all 
-
-    # @skills = Skill.all
-
-    # @educations = Education.all
-
-    # @slides = Slide.all
-
-    # @profile = Profile.first
     @profile = Profile.by_username(params[:username]).first
-    @socials = @profile.socials 
-    @educations = @profile.educations
-    @skills = @profile.skills
-    @slides = @profile.slides
+    # @socials = @profile.socials 
+    # @educations = @profile.educations
+    # @skills = @profile.skills
+    # @slides = @profile.slides
+    respond_to do |format|
+      format.html 
+      format.pdf do
+        html = ApplicationController.renderer.render(partial: 'home/index', locals:{profile: @profile})
+        pdf = WickedPdf.new.pdf_from_string(html)
+        send_data(pdf, filename: "#{params[:username]}.pdf", disposition: "attachment")
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'linked#show'
   # root 'home#index'
-  get '/:username', action: :index, controller: :home
+  get '/:username', action: :index, controller: :home, as: :profile
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
