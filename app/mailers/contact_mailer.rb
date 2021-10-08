9bb7298b68fe8f14aea2e3ce10b0bd57cfa0f293
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-10-08 16:18:03 +0700

    change form to type ruby

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact
     @contact = OpenStruct.new(params[:info])
-    mail(to: @contact.email, subject: "Contact from")
+    mail(to: @contact.email, subject: @contact.subject)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'linked#show'
   # root 'home#index'
   get '/:username', action: :index, controller: :home, as: :profile
+  post '/contact', action: :contact, controller: :home, as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
