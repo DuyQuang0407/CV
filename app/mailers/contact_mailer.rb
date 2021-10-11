a789f2ab84b18b23e4d2bcbfb6e090cfafca61d7
Quang Cao <quangcdse140367@fpt.edu.vn>
2021-10-11 19:36:33 +0700

    finish sidekiq

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,5 +4,6 @@ class ContactMailer < ApplicationMailer
   def contact
     @contact = OpenStruct.new(params[:info])
     mail(to: @contact.email, subject: @contact.subject)
+    params[:job].finished!
   end
 end
